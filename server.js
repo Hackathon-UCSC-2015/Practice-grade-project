044bf2ed6376ec68379f0afdfaff31aee5a6498f
pherniman <pherniman@gmail.com>
2015-01-08 12:01:14 -0800

    Added simple onconnection and onmessage to server.js

--- server.js
+++ server.js
@@ -6,9 +6,20 @@ app.listen(3000);
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({port:8080});
 
+//Sends a message to all users
 wss.broadcast = function(data){
 	wss.clients.forEach(function(client){
 		client.send(data);
 	});
 };
 
+var numberconnected = 0;
+
+wss.on('connection', function(ws){
+	console.log("User "+numberconnected+" connected.");
+	ws.userId = numberconnected++;
+	ws.on('message', function(message){
+		console.log("User "+numberconnected+" said: "+message);
+		ws.send("Thanks for sending a message!");
+	}
+}
